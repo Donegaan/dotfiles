18f98b4843eef927929f5be65c85793158d9b2fc
Andrew D <24252685+Donegaan@users.noreply.github.com>
2022-05-19 20:37:09 +0100

    Added life journal
    Added jrnl config to dotfiles

--- journal_export.sh
+++ journal_export.sh
@@ -3,7 +3,9 @@
 cd ~/workspace/journal
 
 jrnl --format md --file "$OSTYPE"_journal.md
+jrnl life --format md --file "$OSTYPE"_life_journal.md
 
 git add "$OSTYPE"_journal.md
+git add "$OSTYPE"_life_journal.md
 git commit -m "Adding journal entries"
 git push
