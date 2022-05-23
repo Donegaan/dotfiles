1006e6383b60857942de2552939c146b6d345fcc
Andrew Edgescan <andrew.d@edgescan.com>
2022-05-23 09:28:10 +0100

    Update journal cron to use exact path

--- journal/journal_export.sh
+++ journal/journal_export.sh
@@ -4,9 +4,10 @@ cd ~/workspace/journal
 
 mkdir -p work life
 
-jrnl --format md --file work/"$OSTYPE"_journal.md
-jrnl life --format md --file life/"$OSTYPE"_life_journal.md
+/home/linuxbrew/.linuxbrew/bin/jrnl --format md --file work/"$OSTYPE"_journal.md
+/home/linuxbrew/.linuxbrew/bin/jrnl life --format md --file life/"$OSTYPE"_life_journal.md
 
+git pull
 git add work/"$OSTYPE"_journal.md
 git add life/"$OSTYPE"_life_journal.md
 git commit -m "Adding journal entries"
