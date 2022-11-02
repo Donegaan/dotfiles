2cddd1878de1a6da63199f43bbd794ac0527af21
Andrew Edgescan <andrew.d@edgescan.com>
2022-11-02 10:55:33 +0000

    Update treesitter config

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,17 +1,28 @@
+local status_ok, treesitter = pcall(require, "nvim-treesitter")
+if not status_ok then
+	return
+end
+
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
 	return
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
+  ensure_installed = { "lua", "ruby", "javascript"}, -- put the language you want in this array
 	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+
+  highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python", "css" } },
+	indent = { enable = true },
+
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 })
