4b26800534caf0527c2920d1822a0bb25951a896
Andrew D <24252685+Donegaan@users.noreply.github.com>
2022-11-06 22:11:50 +0000

    Add email to github setup script. Update tool versons filename

--- github_setup.sh
+++ github_setup.sh
@@ -2,7 +2,7 @@
 
 # Take email as arg
 
-ssh-keygen -t ed25519 -C "${EMAIL}" - ask for this at start of script
+ssh-keygen -t ed25519 -C "24252685+Donegaan@users.noreply.github.com" 
 eval "$(ssh-agent -s)"
 sh-add ~/.ssh/id_ed25519
 
