761043c0743fc02dfa7f7a263264b8efe6aac696
Andrew Edgescan <andrew.d@edgescan.com>
2022-11-08 16:10:35 +0000

    Update lvim config. Add whichkey mappings. Add few plugins.

--- lvim/config.lua
+++ lvim/config.lua
@@ -12,17 +12,15 @@ vim.opt.colorcolumn = "100"
 vim.opt.scrolloff = 5 -- Determines the number of context lines you would like to see above and below the cursor
 vim.opt.ignorecase = true -- Ignore case in search
 vim.opt.smartcase = true -- Case-sensitive search when search term contains uppercase characters. Otherwise, case-sensitive search.  timeoutlen = 200, -- Time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.cmdheight = 1
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "tokyonight-moon"
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
@@ -46,20 +44,56 @@ lvim.builtin.telescope.defaults.mappings = {
 }
 
 -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
+-- If I want to toggleterm with command, copy Astrovim function: https://github.com/AstroNvim/AstroNvim/blob/80f650ab0c7dc3a93c9fe372010919be936d7eb9/lua/core/utils/init.lua#L283
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["sw"] = { "<cmd>Telescope live_grep<CR>", "Search words" }
+lvim.builtin.which_key.mappings["sW"] = { "<cmd>Telescope live_grep --hidden --no-ignore<CR>",
+  "Search words in all files" }
+lvim.builtin.which_key.mappings["st"] = {
+  "<cmd>lua require'telescope'.extensions.project.project{}<CR>", "Projects"
+}
+lvim.builtin.which_key.mappings["p"] = {
+  name = "+Package",
+  I = { "<cmd>Mason<CR>", "Mason Installer" },
+  U = { "<cmd>MasonToolsUpdate<CR>", "Mason Update" },
+  c = { "<cmd>PackerCompile<cr>", "Compile" },
+  i = { "<cmd>PackerInstall<cr>", "Install" },
+  r = { "<cmd>lua require('lv-utils').reload_lv_config()<cr>", "Reload" },
+  s = { "<cmd>PackerSync<cr>", "Sync" },
+  S = { "<cmd>PackerStatus<cr>", "Status" },
+  u = { "<cmd>PackerUpdate<cr>", "Update" },
+}
 
--- TODO: User Config for predefined plugins
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+ToggleTerm",
+  f = { "<cmd>ToggleTerm direction=float<cr>", "ToggleTerm float" },
+  h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "ToggleTerm horizontal" },
+  v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "ToggleTerm vertical" },
+}
+lvim.builtin.which_key.mappings["S"] = {
+  name = "+Session",
+  l = { "<cmd>SessionManager! load_last_session<cr>", "Load last session" },
+  s = { "<cmd>SessionManager! save_current_session<cr>", "Save this session" },
+  d = { "<cmd>SessionManager! delete_session<cr>", "Delete session" },
+  f = { "<cmd>SessionManager! load_session<cr>", "Search sessions" },
+  c = { "<cmd>SessionManager! load_current_dir_session<cr>", "Load current directory session" },
+}
+lvim.builtin.which_key.mappings["n"] = {
+  name = "Neotest",
+  a = { "<cmd>lua require('neotest').run.attach()<cr>", "Attach" },
+  f = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Run File" },
+  F = { "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Debug File" },
+  l = { "<cmd>lua require('neotest').run.run_last()<cr>", "Run Last" },
+  L = { "<cmd>lua require('neotest').run.run_last({ strategy = 'dap' })<cr>", "Debug Last" },
+  n = { "<cmd>lua require('neotest').run.run()<cr>", "Run Nearest" },
+  N = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Debug Nearest" },
+  o = { "<cmd>lua require('neotest').output.open({ enter = true })<cr>", "Output" },
+  S = { "<cmd>lua require('neotest').run.stop()<cr>", "Stop" },
+  s = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Summary" },
+}
+-- User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
+lvim.builtin.alpha.active = false
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
@@ -78,7 +112,6 @@ lvim.builtin.treesitter.ensure_installed = {
   "ruby",
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
 -- generic LSP settings
@@ -150,30 +183,48 @@ linters.setup {
 lvim.plugins = {
   { "tpope/vim-rails" },
   {
+    "shatur/neovim-session-manager",
+    requires = 'nvim-lua/plenary.nvim',
+    require('session_manager').setup({
+      autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+    })
+  },
+  {
     'sindrets/diffview.nvim',
     requires = 'nvim-lua/plenary.nvim'
+  },
+  {
+    "nvim-neotest/neotest",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "olimorris/neotest-rspec",
+    },
+    config = function()
+      require('neotest').setup({
+        adapters = {
+          require('neotest-rspec'),
+        }
+      })
+    end
   },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
 -- Autocommand to remember cursor position in file
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
-  callback = function()
-    local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
-    if { row, col } ~= { 0, 0 } and row <= vim.api.nvim_buf_line_count(0) then
-      vim.api.nvim_win_set_cursor(0, { row, 0 })
-      if vim.fn.line("w$") ~= row then
-        vim.cmd("norm! zz")
-      end
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+--   pattern = "*",
+--   callback = function()
+--     local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+--     if { row, col } ~= { 0, 0 } and row <= vim.api.nvim_buf_line_count(0) then
+--       vim.api.nvim_win_set_cursor(0, { row, 0 })
+--       if vim.fn.line("w$") ~= row then
+--         vim.cmd("norm! zz")
+--       end
+--     end
+--   end,
+-- })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "zsh",
   callback = function()
