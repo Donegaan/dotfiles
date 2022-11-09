c115425dde7a3de22ff084d12c13a87d0df8fec3
Andrew D <24252685+Donegaan@users.noreply.github.com>
2022-11-09 23:16:31 +0000

    Update regolith install script.

--- scripts/regolith_20_04_install.sh
+++ scripts/regolith_20_04_install.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-# Register the Regolith public key to your local apt
-wget -qO - https://regolith-desktop.org/regolith.key | sudo apt-key add -
-
-# Add the repository URL to your local apt
-echo deb "[arch=amd64] https://regolith-desktop.org/release-ubuntu-focal-amd64 focal main" | \
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-ubuntu-jammy-amd64 jammy main" | \
 sudo tee /etc/apt/sources.list.d/regolith.list
 
-# Update apt
 sudo apt update
-sudo apt install regolith-desktop
-sudo apt install regolith-look-dracula
-regolith-look set dracula
-regolith-look refresh
-sudo apt install regolith-compositor-picom-glx regolith-control-center regolith-ftue 
-sudo apt remove regolith-i3-config regolith-i3-base-launchers regolith-i3-ilia regolith-i3-next-workspace regolith-i3-navigation regolith-i3-gaps-partial regolith-i3-default-style regolith-i3-workspace-config regolith-i3-resize -y
+sudo apt install regolith-desktop -y
+sudo apt upgrade
+
+sudo apt install regolith-look-dracula -y
+sudo apt install regolith-compositor-picom-glx regolith-control-center -y
+sudo apt install i3xrocks-rofication i3xrocks-info i3xrocks-app-launcher i3xrocks-memory -y
+sudo apt remove regolith-i3-gnome regolith-i3-i3xrocks regolith-i3-gaps-partial -y
