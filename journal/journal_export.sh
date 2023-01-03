0f4618836c67cb3b41647bca51f01a9295b2b3f8
Andrew Edgescan <andrew.d@edgescan.com>
2023-01-03 16:54:00 +0000

    Update journal export to use file for each year

--- journal/journal_export.sh
+++ journal/journal_export.sh
@@ -4,11 +4,10 @@ cd ~/workspace/journal
 
 mkdir -p work life
 
-/home/linuxbrew/.linuxbrew/bin/jrnl --format md --file work/"$OSTYPE"_journal.md
+/home/linuxbrew/.linuxbrew/bin/jrnl -from "this year" --format md --file work/work_journal_"$(date +%Y)".md
 /home/linuxbrew/.linuxbrew/bin/jrnl life --format md --file life/"$OSTYPE"_life_journal.md
 
 git pull
-git add work/"$OSTYPE"_journal.md
-git add life/"$OSTYPE"_life_journal.md
+git add .
 git commit -m "Adding journal entries"
 git push
