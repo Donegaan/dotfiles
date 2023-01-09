88c9c99efca1fc2557aaba97cb3f34afc821852d
Andrew Edgescan <andrew.d@edgescan.com>
2023-01-09 12:58:56 +0000

    Update regolith install script to disable default lock manager

--- scripts/regolith_20_04_install.sh
+++ scripts/regolith_20_04_install.sh
@@ -14,3 +14,6 @@ sudo apt install regolith-look-dracula -y
 sudo apt install regolith-compositor-picom-glx regolith-control-center -y
 sudo apt install i3xrocks-rofication i3xrocks-info i3xrocks-app-launcher i3xrocks-memory -y
 sudo apt remove regolith-i3-gnome regolith-i3-i3xrocks regolith-i3-gaps-partial -y
+
+-- Disable default lock screen manager
+gsettings set org.gnome.gnome-flashback screensaver false
