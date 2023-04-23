feb95b185917abcb5b29177cb50d7fef0a504a6d
Andrew D <24252685+Donegaan@users.noreply.github.com>
2023-04-23 11:13:59 +0100

    Got use plugin files loaded (needed to symlink them), but can't get telescope config to work

--- lvim1.3/config.lua
+++ lvim1.3/config.lua
@@ -37,23 +37,7 @@ lvim.keys.normal_mode["n"] = "nzz"
 
 lvim.keys.insert_mode["jk"] = "<ESC>"
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
-local _, actions = pcall(require, "telescope.actions")
-lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-  -- for normal mode
-  n = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-  },
-}
+reload("user.telescope")
 -- -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -84,7 +68,6 @@ lvim.builtin.which_key.mappings["n"] = {
   S = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Summary" },
 }
 
-reload("user.telescope")
 
 -- lvim.builtin.telescope.defaults.layout_config.width = 0.5
 lvim.builtin.alpha.active = false
