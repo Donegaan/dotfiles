dcaa21fdf715a60417b48746a062fd71f599127a
Andrew Edgescan <andrew.d@edgescan.com>
2023-06-30 16:09:24 +0100

    Use copilot.vim instead of copilot.lua as it actually works

--- lvim/config.lua
+++ lvim/config.lua
@@ -196,18 +196,18 @@ lvim.plugins = {
   { "RRethy/nvim-treesitter-endwise" },
   { "nvim-treesitter/nvim-treesitter-context" },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+    "github/copilot.vim",
     config = function()
-      require("copilot").setup()
+      -- copilot assume mapped
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_no_tab_map = true
     end,
   },
   {
-    "zbirenbaum/copilot-cmp",
-    after = { "copilot.lua" },
+    "hrsh7th/cmp-copilot",
     config = function()
-      require("copilot_cmp").setup()
+      lvim.builtin.cmp.formatting.source_names["copilot"] = "( )"
+      table.insert(lvim.builtin.cmp.sources, 2, { name = "copilot" })
     end,
   },
   {
@@ -254,26 +254,6 @@ lvim.plugins = {
     }
   },
 }
-
-
-local ok, copilot = pcall(require, "copilot")
-if not ok then
-  return
-end
-
-copilot.setup {
-  suggestion = {
-    keymap = {
-      accept = "<c-l>",
-      next = "<c-j>",
-      prev = "<c-k>",
-      dismiss = "<c-h>",
-    },
-  },
-}
-
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- Autocommand to remember cursor position in file
