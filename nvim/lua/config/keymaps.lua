3d6d0ea38562fb02a7bf5429925eea169897b408
Andrew Edgescan <andrew.d@edgescan.com>
2023-07-27 12:33:46 +0100

    Add some keymaps and options from lvim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set(
+  "n",
+  "<leader>sg",
+  ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+  { desc = "Live grep args" }
+)
+
+vim.keymap.set("i", "jk", "<ESC>")
