207d74fbe8cfbf4068a9678d162410c99d771fad
Andrew Edgescan <andrew.d@edgescan.com>
2023-07-27 12:34:07 +0100

    Add keymaps for telescope and telescope_live_grep_args

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local lga_actions = require("telescope-live-grep-args.actions")
+
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
@@ -12,6 +14,39 @@ return {
         require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
       end,
       desc = "Find Plugin File",
+    },
+  },
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-j>"] = "move_selection_next",
+          ["<C-k>"] = "move_selection_previous",
+          ["<C-n>"] = "cycle_history_next",
+          ["<C-p>"] = "cycle_history_prev",
+          ["<C-h>"] = "preview_scroll_left",
+          ["<C-l>"] = "preview_scroll_right",
+        },
+        n = {
+          ["<C-j>"] = "move_selection_next",
+          ["<C-k>"] = "move_selection_previous",
+        },
+      },
+    },
+    extensions = {
+      live_grep_args = {
+        auto_quoting = true,
+        mappings = {
+          i = {
+            ["<C-s>"] = lga_actions.quote_prompt(),
+            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+          },
+        },
+        -- ... also accepts theme settings, for example:
+        -- theme = "dropdown", -- use dropdown theme
+        -- theme = { }, -- use own theme spec
+        -- layout_config = { mirror=true }, -- mirror preview pane
+      },
     },
   },
 }
