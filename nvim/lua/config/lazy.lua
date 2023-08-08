b42527b9a2d72223ba74fff18b9ad32d290fa30e
Andrew Edgescan <andrew.d@edgescan.com>
2023-08-08 16:51:45 +0100

    Add eslint and prettier lazy imports

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,6 +12,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here or import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "plugins" },
