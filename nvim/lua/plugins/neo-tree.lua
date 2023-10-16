d0ebbc18d5369842abef4f1ba17d3d286295e93b
Andrew Edgescan <andrew.d@edgescan.com>
2023-10-16 16:57:02 +0100

    Updated function to copy relative path to clipboard to be more reliable

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -15,12 +15,10 @@ return {
         mappings = {
           ["Y"] = function(state)
             local node = state.tree:get_node()
-            -- local content = node.path
-            -- relative
-            local content = node.path:gsub(state.path, ""):sub(2)
-            vim.fn.setreg('"', content)
-            vim.fn.setreg("1", content)
-            vim.fn.setreg("+", content)
+            local path = node:get_id()
+            local value = vim.fn.fnamemodify(path, ":.")
+            vim.fn.setreg("+", value)
+            print(value)
           end,
         },
       },
