d4474d0faa797b7c91ce8dd33b3ce46fd6fcdded
Andrew Edgescan <andrew.d@edgescan.com>
2023-12-15 15:06:00 +0000

    Remove auto save as it was causing issues

--- nvim/lua/plugins/auto-save.lua
+++ nvim/lua/plugins/auto-save.lua
@@ -1,14 +1,19 @@
-return {
-  "Pocco81/auto-save.nvim",
-  config = function()
-    require("auto-save").setup({
-      execution_message = {
-        message = function() -- message to print on save
-          return ""
-        end,
-        dim = 0.18, -- dim the color of `message`
-        cleaning_interval = 0, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-      },
-    })
-  end,
-}
+return {}
+-- local M = {
+--   "Pocco81/auto-save.nvim",
+-- }
+--
+-- function M.config()
+--   require("auto-save").setup({
+--     trigger_events = { "BufLeave" },
+--     execution_message = {
+--       message = function() -- message to print on save
+--         return ""
+--       end,
+--       -- dim = 0.18, -- dim the color of `message`
+--       -- cleaning_interval = 0, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+--     },
+--   })
+-- end
+--
+-- return M
