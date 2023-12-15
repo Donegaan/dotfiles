e518a38b3308f771c8b06677585696244bfff014
Andrew Edgescan <andrew.d@edgescan.com>
2023-12-15 15:06:16 +0000

    Add copilot generation for markdown files

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -5,6 +5,10 @@ local M = {
 function M.config()
   require("copilot").setup({
     copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/lts/bin/node", -- Node.js version must be > 16.x
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
   })
 end
 
